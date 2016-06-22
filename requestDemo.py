70db9cf2306f3a81e39933f3f39afdb27bca57a4
evil0xff <zhangjinze12345@gmail.com>
2016-06-22 10:57:45 +0800

    write a function

--- requestDemo.py
+++ requestDemo.py
@@ -4,8 +4,15 @@ r = requests.get('https://baidu.com')
 
 tb = requests.get('https://taobao.com')
 
-wb = requests.get('http://weibo.cn/pub')
 
-print wb.text
 
-print wb.headers
+def gettext(url):
+    head = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36',}
+    text = requests.get(url, headers=head).text
+
+    return text
+
+t = gettext('http://www.weibo.cn')
+
+print t
