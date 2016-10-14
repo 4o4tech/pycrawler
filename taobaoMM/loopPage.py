e69f3708517dd4796dad296aa9b9d3badede1ba6
jimze <zhangjinze12345@gmail.com>
2016-10-14 23:59:09 +0800

    prepar add the information into mysql database

--- taobaoMM/loopPage.py
+++ taobaoMM/loopPage.py
@@ -1,10 +1,18 @@
 #/usr/bin/python
 # -*- coding:utf-8 *-
 
+import MySQLdb
 import requests
 import urllib
 from bs4 import BeautifulSoup
 from urllib2 import HTTPError
+
+
+
+sql = 'INSERT INTO `taobaomodle` (`name`,`height`,`weight`,`size`,`bar`, `shose`) VALUES (%s,%s,%s,%s,%s,%s)'
+# sql_tmp = 'INSERT INTO `taobaomodle` (`startip`,`endip`,`country`,`local`) VALUES (%s, %s, %s, %s)'
+values = [(16890112,16891391,"泰国","曼谷"),(16891392,16891647,"泰国","如果硅农"), (16891648,16892159,"泰国","加拉信府")]
+
 
 
 def gettext(url):
@@ -23,10 +31,35 @@ def gettext(url):
 def getInfor(htmltext):
     soup = BeautifulSoup(htmltext, 'html.parser')
     p = soup.find_all('p')
-    p_contents = []
-    for i in p:
-        p_contents.append(i.contents[0])
-    return p_contents
+    contents = []
+    for i in p[:5]:
+        contents.append(i.contents[0])
+    img_url = soup.find('img').get('src')
+    contents.append(img_url)
+    return contents
+
+'''
+height 168CM
+weight 45.0KG
+size 82-59-86
+bar 32B
+shose 37码
+img_url  //gtd.alicdn.com/imgextra/i1/T1S35PXpXeXXb1upjX.jpg
+'''
+
+
+
+
+
+def connect_mysql(db_host="127.0.0.1:3306", user="root",
+                   passwd="Jmz5278",db="practice", charset="utf8"):
+    conn = MySQLdb.connect(host=db_host, user=user, passwd=passwd, db=db, charset=charset)
+    conn.autocommit(True)
+    return conn.cursor()
+db1 = connect_mysql()
+# print db1.execute(sql), db1.lastrowid
+print db1.executemany(sql, values), db1.lastrowid
+
 
 def main():
     url = 'https://mm.taobao.com/self/info/model_info_show.htm?user_id=631300490'
@@ -35,7 +68,7 @@ def main():
 
     html = gettext(url)
     list = getInfor(html)
-    for i in list[:5]:
+    for i in list:
         print i
 
 
